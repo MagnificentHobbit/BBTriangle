c56f5f7dd89a6d2dd95d24f6dcc00e54c2223b0f
Rui Araujo <153828154+MagnificentHobbit@users.noreply.github.com>
2024-10-15 16:48:43 +0100

    teste sem testes
    Com gradle

---
+++ src/main/java/triangle/Main.java
@@ -0,0 +1,8 @@
+package triangle;
+public class Main {
+    public static void main(String[] args) {
+        Triangle test = new Triangle(5, 10, 7);
+
+        System.out.println(test.getArea());
+    }
+  }
